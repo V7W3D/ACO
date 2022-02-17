60557d2506f477c4d56db5d58dc0b9c7a65b19bd
AMIRA yanis <amirayanis4@gmail.com>
2022-02-17 16:34:46 +0100

    ma brache

--- Plateau.java
+++ Plateau.java
@@ -1,86 +1,84 @@
 public class Plateau {
+
     public Tuile[][] plateau = new Tuile[10][30];
     Tuile tuileActu;//on va surement s'en debarrasser car elle sert que quand y'a une seule fourmi
-    boolean hungry = true;//n'a pas trouvé de nourririturre
+    boolean hungry = true;//n'a pas trouvé de nouriturre
     public Plateau(){
-    for(int i = 0;i<10;i++){
-        for(int j = 0;j<30;j++){
-            this.plateau[i][j]=new Tuile();
+        for(int i = 0;i<10;i++){
+            for(int j = 0;j<30;j++){
+                this.plateau[i][j]=new Tuile();
+            }
         }
-    }
-//Tuiles accessibles(la ca part dans toutes les directions)
-    for(int i = 0;i<10;i++){
-        for(int j = 0;j<30;j++){
-            if(i+1<10 && j+1<30){
-            plateau[i][j].addAsAdj(plateau[i+1][j+1]);
-            }
-            if(i-1>=0 && j+1<30){
-                plateau[i][j].addAsAdj(plateau[i-1][j+1]);
-            }
-            if(j+1<30){
-                plateau[i][j].addAsAdj(plateau[i][j+1]);
-            }
-              if(i+1<10 && j-1>=0){
-                plateau[i][j].addAsAdj(plateau[i+1][j-1]);
+        //Tuiles accessibles(la ça part dans toutes les directions)
+        for(int i = 0;i<10;i++){
+            for(int j = 0;j<30;j++){
+                if(i+1<10 && j+1<30){
+                    plateau[i][j].addAsAdj(plateau[i+1][j+1]);
                 }
-            if(i-1>=0 && j-1>=0){
+                if(i-1>=0 && j+1<30){
+                    plateau[i][j].addAsAdj(plateau[i-1][j+1]);
+                }   
+                if(j+1<30){
+                    plateau[i][j].addAsAdj(plateau[i][j+1]);
+                }
+                if(i+1<10 && j-1>=0){
+                    plateau[i][j].addAsAdj(plateau[i+1][j-1]);
+                }
+                if(i-1>=0 && j-1>=0){
                     plateau[i][j].addAsAdj(plateau[i-1][j-1]);
                 }
-            if(j-1>=0){
+                if(j-1>=0){
                     plateau[i][j].addAsAdj(plateau[i][j-1]);
                 }
-            if(i-1>=0){
+                if(i-1>=0){
                     plateau[i][j].addAsAdj(plateau[i-1][j]);
                 }
-            if(i+1<10){
+                if(i+1<10){
                     plateau[i][j].addAsAdj(plateau[i+1][j]);
                 }
+            }
         }
+        plateau[3][0].setColony(true);
+        plateau[3][20].setFood(true);
+        tuileActu = plateau[3][0];
+        Fourmi test = new Fourmi();
+        plateau[3][0].Fourmis.add(test);
+
     }
 
-    plateau[3][0].setColony(true);
-    plateau[3][20].setFood(true);
-    tuileActu = plateau[3][0];
-    Fourmi test = new Fourmi();
-    plateau[3][0].Fourmis.add(test);
-
-}
-public void moveAntFrom(Tuile z){
+    public void moveAntFrom(Tuile z){
     //choisie une tuile au hazard c'est la qu'on va rajouter le concepte de pheromone
-    int tuileAccessible = z.tuiles.size();
-    tuileActu.dejaVisite=true;
-    int choix=(int)(Math.random()*tuileAccessible);
-    Tuile x = z.tuiles.get(choix);
-    //La fourmi change de tuile
-    x.tuiles.remove(z);   
-    x.Fourmis.add(z.Fourmis.get(0));
-    z.Fourmis.remove(0);
-    tuileActu = x;
-  /* if(x.Fourmis.get(0).parcours.contains(x)){     //ELIMINER LES BOUCLES DANS LE PARCOURS
-        int i = 0;
-        int k = -1;
-        for(Tuile t : x.Fourmis.get(0).parcours){
-            if(t.equals(x)) k = i;
-            else i++;
-        }
-        for(int l = k;l<x.Fourmis.get(0).parcours.size();i++){
-            x.Fourmis.get(0).parcours.remove(l);
-        }
-    }*/
-    x.Fourmis.get(0).parcours.add(x);
-    System.out.println("la taille du parcours est :" + x.Fourmis.get(0).parcours.size());
-}
+        int tuileAccessible = z.tuiles.size();
+        tuileActu.dejaVisite=true;
+        int choix=(int)(Math.random()*tuileAccessible);
+        Tuile x = z.tuiles.get(choix);
+        //La fourmi change de tuile
+        x.tuiles.remove(z);   
+        x.Fourmis.add(z.Fourmis.get(0));
+        z.Fourmis.remove(0);
+        tuileActu = x;
+    /*  if(x.Fourmis.get(0).parcours.contains(x)){     //ELIMINER LES BOUCLES DANS LE PARCOURS
+            int i = 0;
+            int k = -1;
+            for(Tuile t : x.Fourmis.get(0).parcours){
+                if(t.equals(x)) k = i;
+                else i++;
+            }
+            for(int l = k;l<x.Fourmis.get(0).parcours.size();i++){
+                x.Fourmis.get(0).parcours.remove(l);
+            }
+        }*/
+        x.Fourmis.get(0).parcours.add(x);
+        System.out.println("la taille du parcours est :" + x.Fourmis.get(0).parcours.size());
+    }
     
-
-
-
     public void affiche(){
         for(int i = 0;i<10;i++){
             System.out.println();
             for(int j = 0;j<30;j++){
                 System.out.print(plateau[i][j]);
-    }
-}
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -98,8 +96,7 @@ public void moveAntFrom(Tuile z){
             System.out.println(z.id);
             z.Pherom++;
         }
-        a.affiche();
-        
+        a.affiche(); 
     }
 
 }
