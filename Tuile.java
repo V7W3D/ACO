7e98d3b35e456299177f72547298b9338fc1688b
SIDI AHMED ahmed <sidiahmedahmed2001@gmail.com>
2022-02-17 15:36:23 +0100

    implémentation de l'interface graphique

--- Tuile.java
+++ Tuile.java
@@ -1,43 +1,115 @@
+
 import java.util.ArrayList;
+import java.awt.*;
+
 public class Tuile{
-int Pherom=0;
-static int IDEN=0;
-int id;
-ArrayList<Fourmi> Fourmis = new ArrayList<>();
-boolean isColony = false;
-boolean isFood = false;
-boolean isObstacle = false;
-int tauxEvaportaion;
-int temps=5000;
-ArrayList<Tuile> tuiles = new ArrayList<>();
-boolean dejaVisite = false;
 
-public Tuile(){
-id=IDEN;
-IDEN++;
-}
+    private static int distancemax = 500;
+    private double pherom;//initial value to 1
+    static int IDEN=0;
+    int id;
+    ArrayList<Fourmi> Fourmis = new ArrayList<>();
+    boolean isColony = false;
+    private boolean isFood = false;
+    boolean isObstacle = false;
+    ArrayList<Tuile> tuiles = new ArrayList<>();
+    boolean dejaVisite = false;
+    private int cost = 1;//le cout de la tuile
+    private boolean hasAnt = false;
+    private String idAnt = "";
+    private int i,j;
+    private Vue vue;
+    
+    public Tuile(int i, int j, Vue vue){
+        id=IDEN;
+        IDEN++;
+        pherom = (double) 1 / distancemax;
+        this.setI(i); this.setJ(j);
+        this.vue = vue;
+    }
 
+    public int getJ() {
+        return j;
+    }
 
+    public void setJ(int j) {
+        this.j = j;
+    }
 
-public void setColony(boolean isColony) {
-    this.isColony = isColony;
-}
+    public int getI() {
+        return i;
+    }
 
-public void addAsAdj(Tuile x ){
-    tuiles.add(x);
-}
-public void setFood(boolean isFood) {
-    this.isFood = isFood;
-}
-public boolean thereAreAnts(){
-    return (Fourmis.size()>0);
+    public void setI(int i) {
+        this.i = i;
+    }
 
-}
-public String toString(){
-    if(isColony) return"|C|";
-    if(isFood) return "|F|";
-    if (thereAreAnts()) return "|.|";
-    return"| |";
-}
+    public void setAntID(String idAnt){
+        this.idAnt = idAnt;
+    }
+
+    public boolean hasAnt(){
+        return hasAnt;
+    }
+
+    public void setAnt(boolean hasAnt){
+        this.hasAnt = hasAnt;
+    }
+
+    public int getCost(){
+        return cost;
+    }
+
+    @Override
+    public boolean equals(Object t){
+        return ((Tuile)t).id == id;
+    }
+
+    public boolean isFood(){
+        return isFood;
+    }
+
+    public double getPherom(){
+        return pherom;
+    }
+
+    public void setPherom(float pherom){
+        this.pherom = pherom;
+    }
+
+    public void setColony(boolean isColony) {
+        this.isColony = isColony;
+    }
+
+    public void addAsAdj(Tuile x ){
+        tuiles.add(x);
+    }
+
+    public void setFood(boolean isFood) {
+        this.isFood = isFood;
+    }
+
+    public String toString(){
+        if(isColony) return"|C|";
+        if(isFood) return "|F|";
+        if (hasAnt) return "|"+idAnt+"|";
+        return"| |";
+    }
+
+    public void vaporate(double taux){
+        if(this.pherom > 1/distancemax) {
+            this.pherom = (double) this.pherom * (1 - taux);
+            vue.mesTuiles[i][j].setBackground(vue.mesTuiles[i][j].getBackground().brighter());
+            vue.revalidate();
+            vue.repaint();
+        }
+    }
+
+    public void addPherom(double pherom){
+        this.pherom += pherom;
+        vue.mesTuiles[i][j].setBackground(vue.mesTuiles[i][j].getBackground().darker());
+        vue.revalidate();
+        vue.repaint();
+    }
 
 }
