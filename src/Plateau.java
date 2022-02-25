66315325b41d8a33b15314a01d6fc0988aec29ae
cherifir <rcherifi992@gmail.com>
2022-02-25 10:31:25 +0100

    correction des valeurs

--- src/Plateau.java
+++ src/Plateau.java
@@ -13,7 +13,7 @@ public class Plateau {
     private int height,width;
     private static int delay = 5;
     private static int alpha = 4,beta = 0;
-    private static double tauxDeVaporation = 0.03;
+    private static double tauxDeVaporation = 0.1;
     private int[] tuileDeDepart = new int[2];
     private int nombreFourmis = 5;
     private int nbEclaireuses = 5;
