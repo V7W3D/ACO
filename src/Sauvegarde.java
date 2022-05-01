50e1325e8c54b554e178e117cf19bfe7a9eca81c
SIDI AHMED ahmed <sidiahmedahmed2001@gmail.com>
2022-05-01 11:42:41 +0200

    Ajout de la classe Sauvegarde

---
+++ src/Sauvegarde.java
@@ -0,0 +1,20 @@
+package src;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.awt.Color;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileSystemView;
+
+public class Sauvegarde {
+    private Vue v;
+
+    public Sauvegarde(Vue v) {
+        this.v = v;
+    }
+}
